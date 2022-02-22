2350e73409fcb185e57900a519586ee7c692a3d8
Diego Delgado <dackuli@live.com>
2022-02-21 21:13:26 -0300

    Component compound listo

--- src/01-lazyload/pages/LazyPage2.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const LazyPage2 = () => {
-  return (
-    <h1>Lazy Page2</h1>
-  )
-}
-
-export default LazyPage2
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,45 +6,29 @@ import {
   NavLink,
   Redirect
 } from 'react-router-dom';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 import logo from '../logo.svg';
-import { routes } from './routes';
 
 export const Navigation = () => {
-  console.log(routes)
   return (
-    <Suspense fallback={ <span>Loading...</span>} >
-      <Router>
-        <div className="main-layout">
-          <nav>
-              <img src={ logo } alt="React Logo" />
-            <ul>
-              {
-                routes.map(({path, name},index) => (
-                  <li key={index}>
-                    <NavLink exact to={path} activeClassName="nav-active">{name}</NavLink>
-                  </li>
-                ))
-              }
-            </ul>
-          </nav>
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="nav-active">Shopping</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            {
-              routes.map(({path, Component},index) => (
-                <Route 
-                key={index}
-                path={path} 
-                render={() => <Component />}
-                />
-                ))
-              }
-            <Redirect to={routes[0].path} />
-          </Switch>
-        </div>
-      </Router>
-    </Suspense>
+        <Switch>
+          <Route path="/" render={() => <ShoppingPage />}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
